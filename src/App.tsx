fda21b86f9c36d12e4ca7f165b2cee3d7c83b53d
Daniilkrn <karndaniil@gmail.com>
2026-04-09 18:12:30 +0500

    chore: init project structure

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <main className={styles.root}>
+      <h1>VerificationCodeInput</h1>
+    </main>
+  );
+}
+
+export default App;
